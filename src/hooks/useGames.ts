be1f404334733018a70c54c65813b874360522f7
Mary <maryamkmndi8@gmail.com>
2024-06-08 16:38:08 +0330

    Create a generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-export interface Platform{
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -11,34 +9,9 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: { platform : Platform }[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
-  }
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames =()=>{
-    const controller = new AbortController();
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-    const[isLoading, setLoading] = useState(false);
-    useEffect(() => {
-      setLoading(true);
-      apiClient
-        .get<FetchGamesResponse>("/games",{signal: controller.signal})
-        .then((res) => {
-          setGames(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) =>{
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-          setLoading(false)
-        });
-        // return () => controller.abort();
-    },[]);
-    return {games, error, isLoading}
 }
+
+const useGames = () => useData<Game>("/games");
 export default useGames;
